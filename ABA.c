b7d9e82bd39438b8ab4b749d5cdd8e8ae2999141
SathvikVK <112049518+SathvikVK@users.noreply.github.com>
2022-08-25 09:20:37 +0530

    Create ABA.c

---
+++ ABA.c
@@ -0,0 +1,28 @@
+/*
+Date:25.08.2022
+Author:Sathvik V Koushik
+Title: C Program to Find the Frequency of Characters in a String
+*/
+
+
+
+
+#include <stdio.h>
+int main() {
+    char str[1000], ch;
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to find its frequency: ");
+    scanf("%c", &ch);
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (ch == str[i])
+            ++count;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+    return 0;
+}
